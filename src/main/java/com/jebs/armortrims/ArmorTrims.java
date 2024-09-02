528fa1cc9e6b2dbeefa9c9a23575be93a59da1e2
JebsDeveloper <karl.rockington@gmail.com>
2024-09-02 15:31:48 +0800

    Initial Commit

---
+++ src/main/java/com/jebs/armortrims/ArmorTrims.java
@@ -0,0 +1,17 @@
+package com.jebs.armortrims;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArmorTrims extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
